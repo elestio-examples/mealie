961357c5ea0819d398e6879c9032911d8d719979
amsellemJoseph <you@example.com>
2022-10-23 10:29:10 +0300

    fixe

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -68,7 +68,7 @@ sleep 10s;
   -H 'Accept: application/json, text/plain, */*' \
   -H Referer: http://$target/ \
   -H 'sec-ch-ua-mobile: ?0' \
-  -H 'Authorization: Bearer ${jwt}' \
+  -H Authorization: Bearer $jwt \
   -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36' \
   -H 'sec-ch-ua-platform: "macOS"' \
   --compressed
