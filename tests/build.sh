8aa6852956291138af001107de9e89063b5a8a44
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-05-08 13:33:48 +0300

    tests

--- tests/build.sh
+++ tests/build.sh
@@ -1,7 +1,6 @@
 cd docker;
 
-# mv omni.Dockerfile Dockerfile
-# mv frontend.Dockerfile frontend
+mv omni.Dockerfile Dockerfile
+mv frontend.Dockerfile frontend
 
-# docker build . --tag elestio4test/mealie:latest
-docker build --target production -f ./docker/omni.Dockerfile ../ --tag elestio4test/mealie:latest
+docker build . --tag elestio4test/mealie:latest
